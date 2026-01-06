0fb1b261f28412349cdc10ed2e47b2a25d60106e
Preston Macy <preston.macy@gmail.com>
2026-01-05 22:08:20 -0500

    cleanup: does not emit tsc build, only checks types

--- edge/src/index.template.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=index.template.d.ts.map
--- edge/src/transform.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function injectTypename(input: string): string;
-export declare function getOperationType(query: string): "query" | "mutation" | "subscription" | null;
-export declare function getOperationName(query: string): string | null;
-//# sourceMappingURL=transform.d.ts.map
